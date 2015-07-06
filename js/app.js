730c41b90964a7859333bf4e7495be1f4763a655
Jeffrey Grinberg <grinbergjeff@gmail.com>
2015-07-05 23:25:12 -0400

    slimmed code down

--- js/app.js
+++ js/app.js
@@ -1,33 +1,27 @@
 $(document).ready(function(){	
 // Declare objects (questions, answers)
 	var questions = [
-		{ _id: 0,
-		 question: "On what day was same-sex marriage legalized for all 50 states in the USA?",
+		{ question: "On what day was same-sex marriage legalized for all 50 states in the USA?",
 		 answers: ['June 23rd, 2015','June 24th, 2015','June 25th, 2015','June 26th, 2015','June 27th, 2015'],
 		 correctAnswer: 'June 26th, 2015'
 		},
-		{ _id: 1,
-		 question: "Who was the swing vote?",
+		{ question: "Who was the swing vote?",
 		 answers: ['Justice Kennedy','Justice Ginsburg','Justice Breyer','Justice Sotomayor','Justice Kagan'],
 		 correctAnswer: 'Justice Kennedy'
 		},
-		{ _id: 2,
-		 question: "What was the vote tally?",
+		{ question: "What was the vote tally?",
 		 answers: ['6-3','5-4','4-5','3-6','7-2'],
 		 correctAnswer: '5-4'
 		},
-		{ _id: 3,
-		 question: "What was the case called?",
+		{ question: "What was the case called?",
 		 answers: ['Love v. Beshear','Tanco v. Haslam','DeBoer v. Snyder','Bourke v. Beshear','Obergefell v. Hodges'],
 		 correctAnswer: 'Obergefell v. Hodges'
 		},
-		{ _id: 4,
-		 question: "Which amendment of the U.S. constitution was same-sex marriage declared a fundamental right?",
+		{ question: "Which amendment of the U.S. constitution was same-sex marriage declared a fundamental right?",
 		 answers: ['First Amendment','Tenth Amendement','Eighteenth Amendment','Fourteenth Amendment','Nineteenth Amendment'],
 		 correctAnswer: 'Fourteenth Amendment'
 		},
-		{ _id: 5,
-		 question: "What was the last line of the concluding paragraph from the majority opinion?",
+		{ question: "What was the last line of the concluding paragraph from the majority opinion?",
 		 answers: ['They ask for equal dignity in the eyes of the law.','The Constitution grants them that right.','It is <i>so</i> ordered.','God bless America.','Same Love.'],
 		 correctAnswer: 'It is so ordered.'
 		}];
@@ -60,10 +54,8 @@ function insertquestion(arrayname, qnumber) {
 	for (var i = 0; i < arrayname.length; i++){
 		if (i === qnumber) {
 			$('#qnum').empty().append('Question ' + (i + 1));
-			//console.log('loading question ' + i);
 			$('.questions').prepend('<h2 id="q">' + arrayname[i].question + '</h2>');
 		for (var j = 0; j <= 4; j++) {
-			//console.log('loading answer' + arrayname[i].answers[j]);
 			$('#answers').append('<h3 class="anschoice">' + arrayname[i].answers[j] + '</h3>');
 			}
 		}
@@ -72,7 +64,7 @@ function insertquestion(arrayname, qnumber) {
 }
 // Function to select answer and check answer after hitting next button
 function checkanswer(arrayname, qnumber) {
-	//Indicated Selected Answer
+	//Indicate Selected Answer
 	$('#answers').on('mousedown', '.anschoice',function() {
 		$(this).removeClass().addClass('select');
 		$(this).siblings().removeClass().addClass('anschoice');
